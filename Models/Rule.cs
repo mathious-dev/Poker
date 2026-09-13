08f9c4b9f8ade966d5233f57ab2eb2d82eddbebb
Mathieu-c360 <Mathieu.Colle@Condorcet.be>
2026-09-13 15:27:47 +0200

    feat : ajout des méthodes de vérification de pair ou brelan

--- Models/Rule.cs
+++ Models/Rule.cs
@@ -1,4 +1,6 @@
-﻿namespace Poker;
+﻿using Microsoft.VisualBasic;
+
+namespace Poker;
 
 public class Rule
 {
@@ -18,24 +20,90 @@ public class Rule
     public RuleEnum combination{get;set;}
     public (List<Player>,Player) RuleHightCard(List<Player> players)
     {
-        List<Player> listPlayersWithSameHigh=new List<Player>();
+        var winners=new List<Player>();
         int higherCard=2;
-        Player winner=new Player();
-        foreach(Player player in players)
+        var winner=new Player();
+        foreach(var player in players)
         {
             foreach(Card card in player.Deck)
             {
                 if(card.Number>higherCard)
                 {
-                    higherCard=card.Number;
-                    winner=player;
+                    higherCard=card.Number;   
+                    winner=player;      
+                    winners.Clear();
+                }
+                else if(card.Number==higherCard&& player!=winner)
+                {
+                    winners.Add(player);
                 }
             }
+
         }
-        return (listPlayersWithSameHigh,winner);
+        return (winners,winner);
     }
-    public (List<Player>,Player) RulePair(List<Player>players,int[] cardPlace)
+    public (List<Player>,Player) RulePair(List<Player>players,List<Card> cardPlace)
     {
-    
+        var winners=new List<Player>();
+        var winner=new Player();
+        int highestPair=0;
+        foreach(var player in players)
+        {
+            var allCards=GroupCards(player.Deck,cardPlace);
+            
+            var Pair=SearchPairOrThreeSame(allCards,2);
+            if(Pair.First()>highestPair)
+            {
+                highestPair=Pair.First();
+                winner=player;
+                winners.Clear();
+            }
+            else if(Pair.First()==highestPair)
+            {
+                winners.Add(player);
+            }
+           
+            
+            
+        }
+        return (winners,winner);
+    }
+    public (List<Player>,Player) RuleThreeSameKind(List<Player>players,List<Card> cardPlace)
+    {
+        var winners=new List<Player>();
+        var winner=new Player();
+        int highestThree=0;
+        foreach(var player in players)
+        {
+            var allCards=GroupCards(player.Deck,cardPlace);
+            var Three=SearchPairOrThreeSame(allCards,3);
+            if(Three.First()>highestThree)
+            {
+                highestThree=Three.First();
+                winner=player;
+                winners.Clear();
+            }
+            else if(Three.First()==highestThree)
+            {
+                winners.Add(player);
+            }
+        }
+        return (winners,winner);
+    }
+    public List<int> SearchPairOrThreeSame(List<Card> allCards,int whereNumber)
+    {
+        var Pair=allCards.GroupBy(c=>c.Number)
+                        .Where(g=>g.Count()>=whereNumber)
+                        .Select(g => g.Key)
+                        .OrderByDescending(n=>n)
+                        .ToList();
+        return Pair;
+    }
+    public List<Card> GroupCards(Card[] cardsPlayer,List<Card> cardPlace)
+    {
+        var allCards=new List<Card>();
+        allCards.AddRange(cardsPlayer);
+        allCards.AddRange(cardPlace);
+        return allCards;
     }
 }
