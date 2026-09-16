68e0e99318d581662b34b1c06c7eefaf5a1ba319
Mathieu-c360 <Mathieu.Colle@Condorcet.be>
2026-09-16 15:29:42 +0200

    feat : ajout des méthodes de bot et de bluff

--- Models/Bot.cs
+++ Models/Bot.cs
@@ -16,29 +16,149 @@ public class Bot : Player
     }
     public void BotAction(int level,int highestValue,Rule.RuleEnum combinationStart,int maxCoin)
     {
+        
+        int amountBet=DeclarationOfAction( level, highestValue, combinationStart, maxCoin);
+        // Bet();
+    }
+    public bool Bluff(int level)
+    {
+        Random randomBluff=new Random();
+        bool bluff=false;
         switch(level)
         {
-            case 1 : BotLevelEasy(1,highestValue,combinationStart,maxCoin);break;
-            case 2 : BotLevelMiddle(2,highestValue,combinationStart,maxCoin);break;
-            case 3 : BotLevelHard(3,highestValue,combinationStart,maxCoin);break;
+            case 1: BluffBot(11,ref bluff);break;//facile
+            case 2: BluffBot(6,ref bluff);break;//moyen
+            case 3: BluffBot(4,ref bluff);break;//difficile
+        }
+       return bluff;
+    }
+    public void BluffBot(int chanceForBluff,ref bool bluff)
+    {
+        Random randomBluff=new Random();
+        int isBluffing=0;
+        isBluffing=randomBluff.Next(1,chanceForBluff);
+        if(isBluffing==1)
+            bluff=true;
+        else
+            bluff=false;
+    }
+    
+    //il faudra définir en fonction de la mise obligatoire aussi
+    public void BotLevelEasy(int highestValue,int combinationInt,int maxCoin,ref int betMin,ref int betMax)
+    {
+        switch((combinationInt,highestValue))
+        {
+            case(1,>10):
+            betMin=(int)(maxCoin*0.30);
+            betMax=(int)(maxCoin*1);
+            break;
+            case(1,<6):
+            betMin=(int)(maxCoin*0.20);
+            betMax=(int)(maxCoin*0.40);
+            break;
+            case(2,<9):
+            betMin=(int)(maxCoin*0.60);
+            betMax=(int)(maxCoin*0.90);
+            break;
+            case(<3,>9):
+            betMin=(int)(maxCoin*0.70);
+            betMax=(int)(maxCoin*0.90);
+            break;
+            case(>3,>2):
+            betMin=(int)(maxCoin*1);
+            betMax=(int)(maxCoin*1);
+            break;
+            case(<14,>2):
+            betMin=(int)(maxCoin*1);
+            betMax=(int)(maxCoin*1);
+            break;
+            default:
+            betMin=0;
+            betMax=0;
+            ;break;
         }
     }
-    //à finir
-    public void BotLevelEasy(int level,int highestValue,Rule.RuleEnum combinationStart,int maxCoin)
+    //définir chance
+    public void BotLevelMiddle(int highestValue,int combinationInt,int maxCoin,ref int betMin,ref int betMax)
+    {
+        switch((combinationInt,highestValue))
+        {
+            case(1,>10):
+            betMin=(int)(maxCoin*0.30);
+            betMax=(int)(maxCoin*1);
+            break;
+            case(2,<9):
+            betMin=(int)(maxCoin*0.60);
+            betMax=(int)(maxCoin*0.90);
+            break;
+            case(<3,>9):
+            betMin=(int)(maxCoin*0.70);
+            betMax=(int)(maxCoin*0.90);
+            ;break;
+            case(>3,>2):
+            betMin=(int)(maxCoin*1);
+            betMax=(int)(maxCoin*1);
+            break;
+            default:
+            betMin=0;
+            betMax=0;
+            ;break;
+        }
+    }
+    //définir chance
+    public void BotLevelHard(int highestValue,int combinationInt,int maxCoin,ref int betMin,ref int betMax)
+    {
+        switch((combinationInt,highestValue))
+        {
+            case(1,>10):
+            betMin=(int)(maxCoin*0.30);
+            betMax=(int)(maxCoin*1);
+            break;
+            case(1,<6):
+            betMin=(int)(maxCoin*0.20);
+            betMax=(int)(maxCoin*0.40);
+            break;
+            case(2,<9):
+            betMin=(int)(maxCoin*0.60);
+            betMax=(int)(maxCoin*0.90);
+            break;
+            case(<3,>9):
+            betMin=(int)(maxCoin*0.70);
+            betMax=(int)(maxCoin*0.90);
+            break;
+            case(>3,>2):
+            betMin=(int)(maxCoin*1);
+            betMax=(int)(maxCoin*1);
+            break;
+            default:
+            betMin=0;
+            betMax=0;
+            ;break;
+        }
+    }
+    public int DeclarationOfAction(int level,int highestValue,Rule.RuleEnum combinationStart,int maxCoin)
     {
         int combinationInt=(int)combinationStart;
         int amountBet=0;
+        int betMin=0;
+        int betMax=0;
+        bool bluffOrNot=false;
         Random randomBet=new Random();
-        if(combinationInt<3)
-            amountBet=randomBet(maxCoin-20%,maxCoin-50%);
-        // Bet();
-    }
-    public void BotLevelMiddle(int level,int highestValue,Rule.RuleEnum combinationStart,int maxCoin)
-    {
+        if(combinationInt==1)
+            bluffOrNot=Bluff(level);
+        if(!bluffOrNot)
+        {
+            switch(level)
+            {
+                case 1 : BotLevelEasy(highestValue,combinationInt,maxCoin,ref  betMin,ref  betMax);break;
+                case 2 : BotLevelMiddle(highestValue,combinationInt,maxCoin,ref  betMin,ref  betMax);break;
+                case 3 : BotLevelHard(highestValue,combinationInt,maxCoin,ref  betMin,ref  betMax);break;
+            }
+            amountBet=randomBet.Next(betMin,betMax);
+        }
+        else
+            amountBet=maxCoin;
         
-    }
-    public void BotLevelHard(int level,int highestValue,Rule.RuleEnum combinationStart,int maxCoin)
-    {
-        
+        return amountBet;
     }
 }
--- obj/Debug/net8.0/Poker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Poker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Poker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fa9d82aa1c3f9fea6e13855f9ab12c615a6609")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ab107088d254414277ce66786f5762c43f07f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Poker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Poker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
